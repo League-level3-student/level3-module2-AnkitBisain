22543257dcc9ceaf43f4bae6eb77480f2da53ca7
AnkitBisain <league@WTS14.attlocal.net>
2018-10-28 14:28:36 -0700

    started bogosort

--- src/_00_Sorting_Algorithms/BogoSorter.java
+++ src/_00_Sorting_Algorithms/BogoSorter.java
@@ -6,20 +6,47 @@ public class BogoSorter extends Sorter {
 	public BogoSorter() {
 		type = "Bogo";
 	}
-	
-	// Bogo sort is a joke sorting algorithm. It is considered the most 
+
+	// Bogo sort is a joke sorting algorithm. It is considered the most
 	// inefficient sorting algorithm while still maintaining the possibility
 	// of eventually sorting data.
-	
+
 	// It works by following these steps:
 	// STEP 1. Is the array in order?
-	//	if yes, finished; if no, go to step 2.
+	// if yes, finished; if no, go to step 2.
 	// STEP 2. Take two random elements in the array and swap them.
 	// STEP 3. Go back to step 1.
-	
-	//1. Complete the sort method using the Bogo sort algorithm. 
+
+	// 1. Complete the sort method using the Bogo sort algorithm.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
 
+	}
+
+	boolean isInOrder(int[] arr) {
+		int k = 0;
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				k++;
+			}
+		}
+		if (k > 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	void swap2(int[] arr) {
+		Random r = new Random();
+		int a = r.nextInt(arr.length);
+		int b = r.nextInt(arr.length - 1);
+		if (b == a) {
+			b = arr.length;
+		}
+		int k = arr[a];
+		int j = arr[b];
+		arr[a] = j;
+		arr[b] = k;
 	}
 }
--- src/_00_Sorting_Algorithms/_01_SwappingDigits.java
+++ src/_00_Sorting_Algorithms/_01_SwappingDigits.java
@@ -1,6 +1,7 @@
 package _00_Sorting_Algorithms;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,36 +9,60 @@ class _01_SwappingDigits {
 
 	@Test
 	void test() {
-		int[] x = {5, 6};
-		int[] y = {6, 5};
-		swapArrayOfTwo(x);
-		assertArrayEquals(y, x);
-		
-		int[] z = {7, 3, 5, 9, 2};
-		int[] q = {2, 3, 5, 7, 9};
-		sortIntArray(z);
-		assertArrayEquals(q, z);
-		
-		int[] m = {7, 3, 9, 5, 2};
+		int[] x = { 5, 6 };
+		int[] y = { 6, 5 };
+		assertArrayEquals(y, swapArrayOfTwo(x));
+
+		int[] z = { 7, 3, 5, 9, 2 };
+		int[] q = { 2, 3, 5, 7, 9 };
+		assertArrayEquals(q, sortIntArray(z));
+
+		int[] m = { 7, 3, 9, 5, 2 };
 		assertEquals(5, findMiddle(m));
 	}
-	
-	//1. Complete the method so that the first two elements of the array are swapped
-	public static void swapArrayOfTwo(int[] arr) {
-		
+
+	// 1. Complete the method so that the first two elements of the array are
+	// swapped
+	public static int[] swapArrayOfTwo(int[] arr) {
+		int[] newArr = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			newArr[i] = arr[i];
+		}
+		newArr[0] = arr[1];
+		newArr[1] = arr[0];
+		return newArr;
 	}
-	
-	//2. Complete the method so that it sorts the array using a bubble sort.
-	//   Iterate through the array and when you find two elements that are out
-	//   of order, swap them. Repeat this until the array is in order.
-	public static void sortIntArray(int[] arr) {
-		
+
+	public static int[] swapArrayTwo(int[] arr, int index) {
+		int[] newArr = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			newArr[i] = arr[i];
+		}
+		newArr[index] = arr[index + 1];
+		newArr[index + 1] = arr[index];
+		return newArr;
 	}
-	
-	//3. Complete the method so that it finds the middle number in the array.
-	//   *Hint* it helps to sort it first.
-	//   *Double Hint* Use the method you already wrote in step 2 to sort it
+
+	// 2. Complete the method so that it sorts the array using a bubble sort.
+	// Iterate through the array and when you find two elements that are out
+	// of order, swap them. Repeat this until the array is in order.
+	public static int[] sortIntArray(int[] arr) {
+		for (int i = 0; i < (arr.length) * (arr.length); i++) {
+			for (int j = 0; j < arr.length - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					arr = swapArrayTwo(arr, j);
+				}
+			}
+		}
+		return arr;
+	}
+
+	// 3. Complete the method so that it finds the middle number in the array.
+	// *Hint* it helps to sort it first.
+	// *Double Hint* Use the method you already wrote in step 2 to sort it
 	public static int findMiddle(int[] arr) {
-		return 0;
+		int i = (arr.length - 1) / 2;
+		int mid = sortIntArray(arr)[i];
+		return mid;
 	}
 }
